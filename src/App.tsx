653f1e2a8e8192ed0918578ff1b641dad670b702
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-08-08 16:47:21 -0300

    adding useState to links

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import Navbar from "@/scenes/navbar";
-<<<<<<< HEAD
 import { useState } from "react";
 
 function App() {
   const [ selectedPage ,setSelectedPage ] = useState("home");
 
-  return <div className="app bg-gray-20">
-    
-    <Navbar 
+      
+   return 
+   ( <Navbar 
       selectedPage={selectedPage}
       setSelectedPage={setSelectedPage}
-      />
-=======
-
-function App() {
-  return <div className="app bg-gray-20">
-    
-    <Navbar />
->>>>>>> fb700420be0f15d258644693b3715d6eb50a9e1a
-    
-  </div>;
-}
-
+      />)
+   }
 export default App;
