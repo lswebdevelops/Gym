e6b8ded9da8998caccf959fe1548ce3d5bf1b82c
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-08-08 19:52:12 -0300

    added side menu links and end of navbar

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -74,8 +74,44 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
           </div>
         </div>
       </div>
+{/* Mobal Menu modal */}
+{/* if it is mobile and the button is clicked:  */}
+{!isAboveMediumScreens && isMenuToggled && (
+    <div className="fixed right-0 bottom-0 z-40 h-full w-[300px]  bg-primary-100 drop-shadow-xl">
 
-    </nav>
+    {/* close icon  */}
+    <div className="flex justify-end p-12">
+    <button onClick={() => setIsMenuToggled(!isMenuToggled)}
+    >
+        <XMarkIcon className="h-6 w-6 text-gray-400" />
+    </button>
+    </div>
+    {/* Menu items */}
+    <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  /> 
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />  
+                </div>
+    </div>
+)}
+</nav>
   )
 }
 
