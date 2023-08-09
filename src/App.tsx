a2f1b2aedd5ce60bbb8406bd27ec4e922032c951
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-08-08 21:30:58 -0300

    finished home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home"
 import { useEffect, useState } from "react";
 import { SelectedPage } from "./shared/types";
 
@@ -25,7 +26,10 @@ function App() {
 
     <Navbar
       isTopOfPage={isTopOfPage}
-    selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+      selectedPage={selectedPage} 
+      setSelectedPage={setSelectedPage}/>
+      <Home setSelectedPage={setSelectedPage} />
+
 
   </div>;
 }
