c98af91538a1102b2fab720b51177520c1c05c1e
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-08-09 22:10:39 -0300

    adding animation motion to benefits header

--- src/scenes/benefits/Index.tsx
+++ src/scenes/benefits/Index.tsx
@@ -45,12 +45,21 @@ const Benefits = ({ setSelectedPage }: Props) => {
     >
         
         {/* Header */}
-        <div className="md:my-5 md:w-3/5">
+        <motion.div 
+             initial='hidden'
+             whileInView='visible'
+             viewport={{ once: true, amount: 0.5 }}
+             transition={{ duration: 0.5 }}
+             variants={{
+                 hidden: { opacity: 0, x: -50 },
+                 visible: { opacity: 1, x: 0 },
+             }}
+            className="md:my-5 md:w-3/5">
            <HText>MORE THAT JUST A GYM.</HText>
            <p 
            className="my-5 text-sm"
            >We provide world class fitness equipment, trainers and classes to get you to your ultimate fitness goals with ease. We provide true care into each and every member.</p>
-        </div>
+        </motion.div>
         {/* benefits */}
         <motion.div className="mt-5 items-center justify-between gap-8 md:flex"
         initial="hidden"
