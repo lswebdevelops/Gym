06a6f3a04edd4935bc2dd58ad673802d267d1385
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-08-10 20:51:10 -0300

    contactus page finished

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home"
 import Benefits from "@/scenes/benefits"
 import OurClasses from "@/scenes/ourclasses"
-
+import ContactUs from '@/scenes/contactus'
 import { useEffect, useState } from "react";
 import { SelectedPage } from "./shared/types";
 
@@ -34,6 +34,8 @@ function App() {
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+
 
 
 
