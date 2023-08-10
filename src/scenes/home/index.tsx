420e00c0d441f5cb3ae39ca1232d582c16caf902
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-08-09 21:04:06 -0300

    adding scroll tracker to home page

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -22,7 +22,11 @@ const Home = ({ setSelectedPage }: Props) => {
     className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'
     >
         {/* image and main header */}
-        <div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'>
+        <motion.div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'
+                        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+                        
+                        
+                        >
             {/* main header */}
             <div className='z-10 mt-32 md:basis-3/5'>
                 {/* heading */}
@@ -88,7 +92,7 @@ const Home = ({ setSelectedPage }: Props) => {
             >
                 <img src={HomePageGraphic} alt="home-pageGraphic" />
             </motion.div>
-        </div>       
+        </motion.div>       
                  {/*Sponsors   */}
                  {isAboveMediumScreens && (
                     <div className='h-[150px] w-full bg-primary-100 py-10 '>
